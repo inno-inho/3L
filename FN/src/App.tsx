d0b968a20c0956efbbaf2f4c8ebf296e38d910e7
inno-inho <zergling2565@gmail.com>
2025-12-31 14:23:25 +0900

    헤더붙은 MainLayout 만듬

--- FN/src/App.tsx
+++ FN/src/App.tsx
@@ -1,20 +1,38 @@
 import './App.css';
 import React from 'react'; // React import 추가
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
 import { AuthProvider, useAuth } from './context/AuthContext';
+
 import ConnectionTest from './components/test/ConnectionTest';
+import LoginPage from './components/user/LoginPage';
+import MainLayout from './components/layout/MainLayout';
 
 const App: React.FC = () => {
 
 
   return(
     <>
-      <div className="App"></div>
+      <div className="App">
         <Router>
           <AuthProvider>
-            <ConnectionTest />
+            <Routes>
+              {/* 홈페이지 입장하면 제일 먼저 보일 기본페이지, 로그인 페이지 */}
+              <Route path='/' element={<LoginPage />} />
+              
+              {/* test용 */}
+              <Route 
+                path='/test' 
+                element={
+                <MainLayout>
+                  <ConnectionTest />
+                </MainLayout>
+                } 
+              />
+            </Routes>
           </AuthProvider>
         </Router>
+      </div>  
     </>
   )
 }
