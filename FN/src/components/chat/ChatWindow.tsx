aebf0c0e5b043ac61ee138ad540719dc82bc0142
inno-inho <zergling2565@gmail.com>
2026-01-12 16:32:50 +0900

    ChatWindow 컴포넌트 분리 성공

--- FN/src/components/chat/ChatWindow.tsx
+++ FN/src/components/chat/ChatWindow.tsx
@@ -5,6 +5,7 @@ import type { ChatMessageDto, ChatRoomDto } from "../../types/chat";
 import type { User } from "../../context/AuthContext";
 
 import ChatDropdownMenu from "./ChatDropdownMenu";
+import ChatSearchHeader from "./chatSearchHeader";
 
 import more_vert from "@/assets/image/more_vert.png"
 import text from "@/assets/image/text.svg";
@@ -13,6 +14,7 @@ import addReaction from "@/assets/image/addReaction.svg";
 import coconuttalk_bg from "@/assets/image/coconuttalk_bg.png";
 import stat_minus from "@/assets/image/stat_minus.png";
 import search from "@/assets/image/search.png";
+import ChatInputSection from "./ChatInputSection";
 
 interface ChatWindowProps {
     roomInfo: ChatRoomDto;
@@ -38,21 +40,30 @@ const ChatWindow = ({ roomInfo, currentUser }: ChatWindowProps) => {
     const [searchResults, setSearchResults] = useState<string[]>([]);
     const [currentSearchIndex, setCurrentSearchIndex] = useState(-1);
 
+
+
+    // 검색창 활성화 여부
+    const [isSearchMode, setIsSearchMode] = useState(false);
+
     // 각 메시지 엘리먼트를 참조하기 위한 Map Ref(타입 지정)
     const messageRefs = useRef<Map<string, HTMLDivElement>>(new Map());
+
+
+    // 드롭다운 메뉴 상태 관리
+    const [isChatDropdownOpen, setChatIsDropdownOpen] = useState(false);
+    const chatDropdownRef = useRef<HTMLDivElement>(null);
 
     // 최하단으로 스크롤하는 함수
     const scrollToBottom = () => {
         messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
     }
 
-    // 검색창 활성화 여부
-    const [isSearchMode, setIsSearchMode] = useState(false);
-
     // 새 메시지가 올 때마다 자동 스크롤
     useEffect(() => {
         scrollToBottom();
     }, [messages]);
+
+
 
     // 스크롤 위치를 감지하여 버튼 표시 여부 결정
     const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
@@ -65,10 +76,6 @@ const ChatWindow = ({ roomInfo, currentUser }: ChatWindowProps) => {
             setSshowScrollBtn(false);
         }
     }
-
-    // 드롭다운 메뉴 상태 관리
-    const [isChatDropdownOpen, setChatIsDropdownOpen] = useState(false);
-    const chatDropdownRef = useRef<HTMLDivElement>(null);
 
     // 외부 클릭 시 닫기
     useEffect(() => {
@@ -188,126 +195,22 @@ const ChatWindow = ({ roomInfo, currentUser }: ChatWindowProps) => {
     return (
         <>
             <div className="relative flex-1 bg-white rounded-3xl shadow-sm border border-[#E5E0D5] flex flex-col overflow-hidden">
-                {/* 상단 헤더 (통합프레임) */}
-                <div className="h-16 border-b border-gray-100 flex items-center justify-between px-6 bg-white">
-                    {!isSearchMode ? (
-                        // 기본 모드
-                        <>               
-                            <div className="flex items-center gap-3">
-                                {/* roomImage 처리 */}
-                                <div className="w-10 h-10 bg-gray-200 rounded-full overflow-hidden flex items-center justify-center">
-                                    {roomInfo.roomImageUrls && roomInfo.roomImageUrls.length > 0 ? (
-                                        <img
-                                            src={roomInfo.roomImageUrls[0]}
-                                            alt={roomInfo.roomName}
-                                            className="w-full h-full object-cover"
-                                        />
-                                    ) : (
-                                        // 이미지가 없을 때 보여줄 기본 아이콘이나 대체 텍스트
-                                        <img src={coconuttalk_bg} alt="코코넛톡 기본 이미지" />
-                                    )}
-                                </div>
 
-                                {/* 방 이름 및 인원수 and Search Icon */}
-                                <div className="flex items-center">
-                                    <span className="font-bold text-[#4A3F35] leading-none">
-                                        {roomInfo.roomName}
-                                    </span>
-                                    {roomInfo.userCount > 2 && (
-                                        <span className="text-xs text-gray-400 pl-3">
-                                            {roomInfo.userCount}명
-                                        </span>
-                                    )}
-                                    <button
-                                        className="h-8 w-8 pl-3"
-                                        onClick={() => setIsSearchMode(true)}
-                                    >
-                                        <svg className="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                                        </svg>
-                                    </button>
-                                </div>
-                            </div>
-                            {/* 누르면 드롭다운메뉴 나오는 곳 ... */}
-                            <div
-                                className="relative"
-                                ref={chatDropdownRef}
-                            >
-                                <button
-                                    className=" hover:bg-gray-100 w-10 h-10 flex items-center justify-center rounded-full"
-                                    onClick={() => setChatIsDropdownOpen(!isChatDropdownOpen)}
-                                >
-                                    <img src={more_vert} alt="채팅알람 관련 설정" className="w-7 h-7" />
-                                </button>
-                                
-                                {/* 드롭다운 컴포넌트 적용 */}
-                                <ChatDropdownMenu
-                                    isOpen={isChatDropdownOpen}
-                                    chatRoomType={roomInfo.chatRoomType}
-                                />
-                            </div>
-                        </>
-                    ) : (
-                        // 검색창 모드
-                        <>                        
-                            <div className="flex items-center gap-4 w-full animate-in silde-in-from-top-1 duration-200">
-                                {/* 검색창 */}
-                                <div className="flex-1 flex items-center bg-[#F8F6F2] rounded-xl px-4 py-2 border border-transparent focus-within:border-[#B5A492] transition-all">
-                                    <svg className="w-4 h-4 text-gray-400 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                                    </svg>
-                                    <input
-                                        autoFocus
-                                        className="bg-transparent flex-1 outline-none text-sm"
-                                        placeholder="Search"
-                                        value={searchQuery}
-                                        onChange={(e) => handleSearch(e.target.value)}
-                                        onKeyDown={(e) => {
-                                            if (e.key === 'Enter') {
-                                                e.preventDefault();         // 페이지 새로고침 
-                                                
-                                                // 검색 결과가 있는 상태(화살표가 있음)에서 엔터를 치면
-                                                if(searchResults.length > 0){
-                                                    moveSearchIndex('next');    // 엔터 누르면 다음 결과로 이동
-                                                }
-                                                
-                                            }
-                                        }}
-                                    />
-                                    {/* 검색 결과 컨트롤(결과가 있을 때만 표시) */}
-                                    {searchResults.length > 0 && (
-                                        <div className="flex items-center gap-3 border-1 border-gray-300 ml-2 pl-3 text-gray-500">
-                                            <span className="text-[11px] font-medium min-w-[35px]">
-                                                {currentSearchIndex + 1} / {searchResults.length}
-                                            </span>
-                                            <div className="flex gap-1">
-                                                <button onClick={() => moveSearchIndex('prev')} className="p-1 hover:bg-gray-200 rounded-3xl h-5 w-5">
-                                                    <img
-                                                        src={stat_minus}
-                                                        alt="채팅 검색 내용 위로 이동"
-                                                        className="rotate-180"
-                                                    />
-                                                </button>
-                                                <button onClick={() => moveSearchIndex('next')} className="p-1 hover:bg-gray-200 rounded-3xl h-5 w-5">
-                                                    <img
-                                                        src={stat_minus}
-                                                        alt="채팅 검색 내용 아래로 이동"
-                                                    />
-                                                </button>
-                                            </div>
-                                        </div>
-                                    )}
-                                </div>
-                                <button
-                                    onClick={handleCloseSearch}
-                                    className="text-sm text-darkgray-50 hover:text-[#743F24] px-1 transition-colors font-semibold"
-                                >
-                                    X
-                                </button>
-                            </div>            
-                        </>
-                    )}
-                </div>
+                {/* 채팅윈도우의 헤더(검색창 있는 곳) */}
+                <ChatSearchHeader
+                    isSearchMode={isSearchMode}
+                    setIsSearchMode={setIsSearchMode}
+                    roomInfo={roomInfo}
+                    searchQuery={searchQuery}
+                    searchResults={searchResults}
+                    currentSearchIndex={currentSearchIndex}
+                    handleSearch={handleSearch}
+                    moveSearchIndex={moveSearchIndex}
+                    handleCloseSearch={handleCloseSearch}
+                    isChatDropdownOpen={isChatDropdownOpen}
+                    setChatIsDropdownOpen={setChatIsDropdownOpen}
+                    chatDropdownRef={chatDropdownRef}
+                />
 
                 {/* 메시지 리스트 영역 */}
                 <div
@@ -330,12 +233,12 @@ const ChatWindow = ({ roomInfo, currentUser }: ChatWindowProps) => {
                         }
 
                         return (
-                            <div 
+                            <div
                                 key={msg.messageId}
                                 ref={(el) => {
                                     if (el) messageRefs.current.set(msg.messageId, el);
-                                    else messageRefs.current.delete(msg.messageId);    
-                                }}    
+                                    else messageRefs.current.delete(msg.messageId);
+                                }}
                                 className={`flex ${isMine ? 'justify-end' : 'justify-start'}`}
                             >
                                 {!isMine && (
@@ -394,54 +297,11 @@ const ChatWindow = ({ roomInfo, currentUser }: ChatWindowProps) => {
                 )}
 
                 {/* 하단 메시지 입력창 영역 */}
-                <div className="p-4 bg-white border-t border-gray-50">
-                    <div className="border border-[#743F24] rounded-2xl p-3 bg-white">
-                        <textarea
-                            className="w-full h-24 resize-none outline-none text-sm font-semibold py-2 px-2"
-                            placeholder="메시지를 입력해주세요"
-                            value={inputText}
-                            onChange={(e) => setInputText(e.target.value)}  // 입력할 때마다 상태 업데이트
-                            onKeyDown={(e) => {
-                                // Enter키를 누르면 전송 (Shift+Enter는 줄바꿈)
-                                if (e.key === 'Enter' && !e.shiftKey) {
-                                    e.preventDefault();
-                                    handleSend();
-                                }
-                            }}
-                        />
-                        <div className="flex justify-between items-center mt-2">
-                            <div className="flex gap-3">
-                                <button className="hover:bg-gray-200 h-12 w-12 flex items-center justify-center rounded-full">
-                                    <img
-                                        src={addPhoto}
-                                        alt="사진이나 파일 추가"
-                                        className="h-8 w-8"
-                                    />
-                                </button>
-                                <button className="hover:bg-gray-200 h-12 w-12 flex items-center justify-center rounded-full">
-                                    <img
-                                        src={text}
-                                        alt="파일 추가"
-                                        className="h-8 w-8"
-                                    />
-                                </button>
-                                <button className="hover:bg-gray-200 h-12 w-12 flex items-center justify-center rounded-full">
-                                    <img
-                                        src={addReaction}
-                                        alt="리액션"
-                                        className="h-8 w-8"
-                                    />
-                                </button>
-                            </div>
-                            <button
-                                onClick={handleSend}    // 클릭 시 전송 함수 실행
-                                className="bg-[#B5A492] hover:bg-[#8B4513] text-white px-6 py-1.5 rounded-xl text-sm transition-colors font-medium"
-                            >
-                                전송
-                            </button>
-                        </div>
-                    </div>
-                </div>
+                <ChatInputSection
+                    inputText={inputText}
+                    setInputText={setInputText}
+                    handleSend={handleSend}
+                />
             </div>
         </>
     );
