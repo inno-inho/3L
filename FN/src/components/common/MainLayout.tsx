75fa68ef783b11d22e4ca981132a4787628f94c8
rombird <romsae5510@gmail.com>
2026-01-30 14:50:33 +0900

    병합후 작업...

--- FN/src/components/common/MainLayout.tsx
+++ FN/src/components/common/MainLayout.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import type {ReactNode} from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import Header from './Header';
+import { useAuth } from '../../context/AuthContext';
 
-const MainLayout: React.FC = () => {
+interface MainLayoutProps{
+    children?: ReactNode;
+}
+
+const MainLayout: React.FC<MainLayoutProps> = () => {
+    const { user } = useAuth(); 
+
     return(
         <div className='w-full min-h-screen bg-[#fff9ed] flex justify-center'>
             <div className="w-full max-w-screen-xl mx-auto flex flex-col">
                 <Header />
                 <div className="flex-1 flex min-h-0">
-                    <Sidebar />
+                    <Sidebar currentUser={user} />
                     <main className="flex-1 min-w-0 px-4 py-3">
                         <div className="w-full h-full mx-auto bg-white rounded-xl shoadow-sm p-8">
                             <Outlet /> {/* 바뀌는 영역 */}
