be160a97dbc308dcbc4716ee0004d2cedaef9987
inno-inho <zergling2565@gmail.com>
2026-02-14 18:08:02 +0900

    사진 불러오는 문제도 해결?

--- FN/src/components/common/ConfirmModal.tsx
+++ FN/src/components/common/ConfirmModal.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
-import coconuttalk from "@/assets/image/coconuttalk.png";
+import coconuttalk from "@/assets/image/coconuttalk.png"
 
 interface ConfirmModalProps {
     show: boolean;
